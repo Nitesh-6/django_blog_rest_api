560e469976a500f32e4e8a558c7e1f5053823686
Nitesh-6 <sainitesh6@gmail.com>
2024-09-04 12:18:13 +0530

    did all the basic crud operations by using rest framework and functions based views

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from .models import Blog
+from .serializer import BlogSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
 def blog_list(req):
-    data = {
-        "message": "Hello World!"
-    }
-    return JsonResponse(data=data)
+    if req.method == "GET":
+        all_blogs = Blog.objects.filter(is_public=True)
+        serializer = BlogSerializer(all_blogs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    if req.method == "POST":
+        serializer = BlogSerializer(data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def blog_detail(req, pk):
+    if req.method == "GET":
+        blog = Blog.objects.get(is_public=True, pk=pk)
+        serializer = BlogSerializer(blog)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    if  req.method == "PUT":
+        blog = Blog.objects.get(pk=pk)
+        serializer = BlogSerializer(blog, data=req.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if req.method == 'DELETE':        
+        blog = Blog.objects.get(pk=pk)
+        blog.delete()
+        return Response(status=status.HTTP_200_OK)
--- blog_rest_api/settings.py
+++ blog_rest_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'account',
     'blog_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
