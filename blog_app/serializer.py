02833bd53dd77014fd5760605e43a8e370c19893
Nitesh-6 <sainitesh6@gmail.com>
2024-09-17 11:11:35 +0530

    added validations in serializers

--- blog_app/serializer.py
+++ blog_app/serializer.py
@@ -1,22 +1,52 @@
 from rest_framework import serializers
 from .views import Blog
 
+def name_valid(value):
+    if len(value) < 4:
+        raise serializers.ValidationError('Blog title is too short')
+    else:
+        return value
 
 class BlogSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    name= serializers.CharField(validators = [name_valid])
+    description = serializers.CharField()
+    post_date = serializers.DateField(required=False)
+    is_public = serializers.BooleanField()
+    slug = serializers.CharField(required=False)
+    
     class Meta:
         model = Blog
-        # fields = ['name', 'description', 'is_public', 'slug']
         fields = '__all__'
+        # fields = ['name', 'description', 'is_public', 'slug']
         # exclude = ['slug']
+        
+    # Filed-level validation
+    # def validate_name(self, value):
+    #     if len(value) < 4:
+    #         raise serializers.ValidationError('Blog Title is too short')
+    #     else:
+    #         return value
+    # object-level validation
+    
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError('Blog Title and Description cannot be the same')
+        else:
+            return data
+        
+    # validator
+    
+    
 
 # ================================> simple serializers
 # class BlogSerializer(serializers.Serializer):
 #     id = serializers.IntegerField(read_only=True)
 #     name= serializers.CharField()
 #     description = serializers.CharField()
-#     post_date = serializers.DateField()
+#     post_date = serializers.DateField(required=False)
 #     is_public = serializers.BooleanField()
-#     slug = serializers.CharField()
+#     slug = serializers.CharField(required=False)
     
 #     def create(self, validated_data):
 #         return Blog.objects.create(**validated_data)
